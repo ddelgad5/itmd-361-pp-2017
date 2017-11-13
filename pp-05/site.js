dc910d31e4bd3483665bc27eda093f6bcdfa7d6b
David Delgado <ddelgad5@hawk.iit.edu>
2017-11-13 14:27:26 -0600

    Add keydown listener to play doorbell on d key.

--- pp-05/site.js
+++ pp-05/site.js
@@ -6,3 +6,15 @@ jQuery('#doorbell').on('click', function () {
   var doorbell = new Audio('media/doorbell.mp3');
   doorbell.play();
 });
+
+window.addEventListener("keydown", function(event) {
+
+  switch (event.key) {
+    case "d":
+      var doorbell = new Audio('media/doorbell.mp3');
+      doorbell.play();
+    break;
+    default:
+    return;
+  }
+},true);
