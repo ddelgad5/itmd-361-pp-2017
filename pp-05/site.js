fa90e1444f62471c32ed92578d6d8ce965d98248
David Delgado <ddelgad5@hawk.iit.edu>
2017-11-13 14:34:24 -0600

    Reinstate playDoorBell to make code DRY.

--- pp-05/site.js
+++ pp-05/site.js
@@ -1,18 +1,21 @@
+function playDoorBell() {
+  var doorbell = new Audio('media/doorbell.mp3');
+  doorbell.play();
+}
+
 window.onload = function(){
   jQuery(jQuery('audio')).addClass('hide');
 };
 
 jQuery('#doorbell').on('click', function () {
-  var doorbell = new Audio('media/doorbell.mp3');
-  doorbell.play();
+  playDoorBell();
 });
 
 window.addEventListener("keydown", function(event) {
 
   switch (event.key) {
     case "d":
-      var doorbell = new Audio('media/doorbell.mp3');
-      doorbell.play();
+      playDoorBell();
     break;
     default:
     return;
