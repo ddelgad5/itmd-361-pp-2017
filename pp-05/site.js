aea8f9749348598015252a96d517e1e1a633a95c
David Delgado <ddelgad5@hawk.iit.edu>
2017-11-13 14:08:36 -0600

    Add class hide with jQuery.

--- pp-05/site.js
+++ pp-05/site.js
@@ -1,4 +1,8 @@
-function ringDoorbell() {
+window.onload = function(){
+  jQuery(jQuery('audio')).addClass('hide');
+};
+
+jQuery('#doorbell').on('click', function () {
   var doorbell = new Audio('media/doorbell.mp3');
   doorbell.play();
-}
+});
